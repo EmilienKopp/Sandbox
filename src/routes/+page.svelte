8a98723935b4ca4e49216c85d88c380c430c61b2
ekFeliseed <emilien@feliseed.co.jp>
2023-07-24 19:01:19 +0900

    final touch for template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
+<script lang="ts">
+    import { createClient } from '@supabase/supabase-js';
+    import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
+
+    const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+
+    let user: any;
+
+    async function signInWithGoogle() {
+        const {data, error} = await supabase.auth.signInWithOAuth({
+            provider: 'google',
+        })
+        if (error) console.log(error)
+        else {
+            user = data;
+            console.log(user)
+        }
+    }
+
+</script>
+<svelte:head>
+    <title>Home</title>
+</svelte:head>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+
+<button on:click={signInWithGoogle} class="rounded-md bg-blue-400 text-white p-2">Sign in with Google</button>
+
+{#if user}
+  {user}
+{/if}
